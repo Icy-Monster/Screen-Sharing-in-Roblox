50d99212a54ec3488a53b449be394d36dc68b22d
icyCYBERwhynot <102822122+Icy-Monster@users.noreply.github.com>
2023-09-22 15:09:28 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Screen Sharing in Roblox
+
+This project is a simple way of doing screen sharing in Roblox, while trying to keep the quality and performance high.
+
+
+It uses 2 main forms of compression:
+- Removing repetition of color data_
+ (255,255,255), (255,255,255), (255,255,255) -> (255,255,255), 2
+
+ - Removes colors that we're not changed compared to the last frame
+
+ Last Frame:
+  (255,255,255), (255,255,255), (255,255,255)
+
+New Frame: 
+(255,255,255), (250,255,250), (255,255,255)
+
+Output: 
+1.1, (250,255,250), 1.1
+
+Hex is used to store the color, but to display how the compression works I used RGB values
+
+
+
+
+
+Notes:
+-
+
+- I apologize if this is not properly documentated, since the original meaning behind making this was not to publicize it.
+
+- I am by no way a great python programmer, so I once again apologize for the sloppy documentation and code
+
+- Nonetheless I hope you enjoy it and I would especially love if you would even share your work with this and maybe even contribute to it ;)
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Installation
+
+It's quite simple to install this, you only need a way to port forward (I recommend using Ngrok, if you cannot do it yourself)
+
+You will also need to install the Python packages:
+```bash
+pip install -r requirements.txt
+```
+(run the command in the path of the requirements.txt file)
+    
---
+++ Screen.py
@@ -0,0 +1,144 @@
+from flask import Flask, jsonify,request
+from PIL import Image,ImageGrab
+import pydirectinput
+import time
+import cv2
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+####Settings####
+FramesPerSecond = 8 # max fps is framegroups * 8
+XRes = 16*25   # 16*20 #
+YRes = 9*25 # 9*20
+
+KeyInput = True
+
+LosslessColorQuality = True
+FrameGroups = 1 #keep to low amounts such as 3 
+
+FrameSkip = 0
+
+FrameStart = 0
+VideoStreaming = False
+VideoPath = r"video path here"
+####Settings####
+
+LastFrame = []
+FrameCount = 1
+
+ServerList = {}
+
+cap = cv2.VideoCapture(VideoPath)
+
+cap.set(cv2.CAP_PROP_POS_FRAMES,FrameStart)
+
+def RGBToHex(rgb):
+    return f"{(rgb[0] >> 4):X}{(rgb[1]  >> 4):X}{(rgb[2] >> 4):X}"
+
+def EncodeFrame(FirstTime,ServerID,SkipFrame):     
+    global LastFrame,FrameCount,LastFrame2,CurrentFrame,ServerList
+
+    if VideoStreaming and SkipFrame == "1":
+        ServerList[ServerID] += 2
+        cap.set(cv2.CAP_PROP_POS_FRAMES,ServerList[ServerID]);
+    
+    lastpixel = ""
+    lastenumerate,lastDuplicate = 0,0
+    number,DuplicateNumber = 1,1
+    
+    WasDuplicate = False
+    WasRepetitive = False
+    
+    if FirstTime == "1":
+        #Refresh the clients screen (doesnt compress it)
+        LastFrame = []
+
+    if not VideoStreaming:
+        pic = ImageGrab.grab().resize((XRes,YRes),Image.Resampling.BILINEAR)
+    else:
+        playing, frame = cap.read()
+        if not playing:
+            #video ended, go to 1st
+            cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+            ServerList[ServerID] = 0
+            _, frame = cap.read()
+        
+        pic = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).resize((XRes,YRes),Image.Resampling.BILINEAR)
+    
+    if LosslessColorQuality:
+        CurrentFrame = ["%02x%02x%02x" % pixel for pixel in pic.getdata()]
+    else:
+        CurrentFrame = [RGBToHex(pixel) for pixel in pic.getdata()]
+
+    #removes duplicates
+    LastFrame2 = [*CurrentFrame]
+
+    FrameLen = len(LastFrame)
+    
+    for i,v in enumerate(CurrentFrame):
+        #Removes non changed colors
+        if FrameLen > i and v == LastFrame[i]:
+            if WasDuplicate:
+                DuplicateNumber += 1
+                CurrentFrame[lastDuplicate] = DuplicateNumber
+                CurrentFrame[i] = '' 
+            else:
+                CurrentFrame[i] = 1
+                lastDuplicate = i
+                DuplicateNumber = 1
+            WasDuplicate = True
+            continue
+        else:
+            WasDuplicate = False
+        
+        #Removes color repetition
+        if lastpixel == v:
+            if WasRepetitive:
+                number += 1
+                CurrentFrame[lastenumerate] = number
+                CurrentFrame[i] = ''
+            else:
+                CurrentFrame[i] = 1.1
+                lastenumerate = i
+                number = 1.1
+            WasRepetitive = True
+        else:
+            lastpixel = v
+            WasRepetitive = False
+    
+    FrameCount += 1
+    if FrameCount < FrameSkip:
+        LastFrame = []
+    else:
+        LastFrame = LastFrame2
+    
+    return tuple(filter(None, CurrentFrame))
+
+@app.route('/',methods=['POST'])
+def ReturnFrame():
+    global ServerList
+    Method = request.headers["R"]
+    
+    ServerID = request.headers["I"]
+    SkipFrame = request.headers["F"]
+
+    if not ServerID in ServerList:
+        ServerList[ServerID] = FrameStart
+    
+    #Prints all the servers that its keeping track of
+    print(ServerList)
+    
+    Frames = []
+    for _ in range(FrameGroups):
+        time.sleep(1/FramesPerSecond)
+        Frames.append(EncodeFrame(Method,ServerID,SkipFrame))
+    
+    return jsonify(Fr=Frames,F=FramesPerSecond,X=XRes, Y=YRes, G=FrameGroups)
+
+def StartApi(Port):
+    print(str(XRes) + "x" + str(YRes) + "     Port:" + str(Port))
+    Server = WSGIServer(('127.0.0.1', Port), app)
+    Server.serve_forever()
+
+StartApi(1241)
